85e9f0140faa8462376a1e4328f618d0cb99d6f6
Anna Petry <hello@annapetry.com>
2014-06-20 15:10:30 -0700

    Create tests and code for invalid login attempts

--- odot/spec/controllers/user_sessions_controller_spec.rb
+++ odot/spec/controllers/user_sessions_controller_spec.rb
@@ -38,7 +38,54 @@ describe UserSessionsController do
         expect(user).to receive(:authenticate)
         post :create, email: "hello@annapetry.com", password: "password"                  
       end
+      
+      it "sets the user_id in the session" do
+         post :create, email: "hello@annapetry.com", password: "password"
+         expect(session[:user_id]).to eq(user.id)
+      end
+      
+      it "sets the flash success message" do
+        post :create, email: "hello@annapetry.com", password: "password"
+        expect(flash[:success]).to eq("Thanks for logging in!")
+      end
     end
+    
+    shared_examples_for "denied login" do
+      it "renders the new template" do
+        post :create, email: email, password: password
+        expect(response).to render_template('new')
+      end
+      
+      it "sets the flash error message" do
+        post :create, email: email, password: password
+        expect(flash[:error]).to eq("There was a problem loggind in.  Please check you email and password.")
+      end
+    end
+    
+    context "with blank credentials" do
+      let(:email) { "" }
+      let(:password) { "" }
+      it_behaves_like "denied login"
+    end
+      
+    
+    context "with an incorrect password" do
+      let!(:user) { User.create(first_name: "Anna", 
+                    last_name: "Petry", 
+                    email: "hello@annapetry.com", 
+                    password: "password", 
+                    password_confirmation: "password")}
+      let(:email) { user.email }
+      let(:password) { "incorrect" }
+      it_behaves_like "denied login"                    
+    end
+    
+    context "with no email in existance" do
+      let(:email) { "none@found.com" }
+      let(:password) { "incorrect" }
+      it_behaves_like "denied login"
+    end
+    
+    
   end
-
 end
